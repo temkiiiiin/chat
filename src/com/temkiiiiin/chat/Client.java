1d36b9b4e92f8e30959c75052c5e10f08986cb55
temkiiiiin <temkiiiiin@gmail.com>
2017-10-11 21:00:53 +0300

    обработал ввод клиента; отправляю сообщения на сервер

--- src/com/temkiiiiin/chat/Client.java
+++ src/com/temkiiiiin/chat/Client.java
@@ -4,6 +4,7 @@ package com.temkiiiiin.chat;
 import java.io.OutputStream;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Client {
     public static void main(String[] args) {
@@ -11,10 +12,15 @@ public class Client {
             Socket socket = new Socket(InetAddress.getByName("localhost"), 1234);
             OutputStream outputStream = socket.getOutputStream();
 
+            Scanner scanner = new Scanner(System.in);
+
             String message = "";
             do {
+                message = scanner.nextLine();
+                outputStream.write(message.trim().getBytes());
+            } while (!"stop".equals(message.trim()));
 
-            } while (message.trim() != "stop");
+            socket.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
