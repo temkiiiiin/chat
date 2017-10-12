2d16f3dc3a823688bdac8914cc0b95ab616dc7af
temkiiiiin <temkiiiiin@gmail.com>
2017-10-12 07:29:23 +0300

    научил клинета принимать сообщения

--- src/com/temkiiiiin/chat/Client.java
+++ src/com/temkiiiiin/chat/Client.java
@@ -1,16 +1,18 @@
 package com.temkiiiiin.chat;
 
 
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.util.Scanner;
 
-public class Client {
+public class Client extends Thread {
     public static void main(String[] args) {
         try {
             Socket socket = new Socket(InetAddress.getByName("localhost"), 1234);
             OutputStream outputStream = socket.getOutputStream();
+            new Client(socket.getInputStream());
 
             Scanner scanner = new Scanner(System.in);
 
@@ -23,6 +25,32 @@ public class Client {
             socket.close();
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    private InputStream inputStream;
+
+    public Client(InputStream inputStream) {
+        this.inputStream = inputStream;
+        start();
+    }
+
+    @Override
+    public void run() {
+        boolean connect = true;
+        while (connect) {
+            byte[] messageBytes = new byte[1024];
+            try {
+                int messageLen = inputStream.read(messageBytes);
+
+                if (messageLen == -1) {
+                    connect = false;
+                } else {
+                    System.out.println(new String(messageBytes));
+                }
+            } catch (Exception e) {
+                connect = false;
+            }
         }
     }
 }
