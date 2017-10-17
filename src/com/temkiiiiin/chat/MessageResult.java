287d9b63a1938e081abf1ce0f0bb4911faf33a32
temkiiiiin <temkiiiiin@gmail.com>
2017-10-17 22:40:53 +0300

    унаследовал MessageResult от Message; добавил методы сериализации и десериализации Message

--- src/com/temkiiiiin/chat/MessageResult.java
+++ src/com/temkiiiiin/chat/MessageResult.java
@@ -1,24 +1,53 @@
 package com.temkiiiiin.chat;
 
+import java.io.*;
+import java.util.Date;
+
 enum MessageStatus{
     OK,
     DISCONNECT
 }
 
-public class MessageResult {
+public class MessageResult extends Message implements Serializable {
     private MessageStatus status;
-    private String text;
 
     public MessageStatus getStatus() {
         return status;
     }
 
-    public String getText() {
-        return text;
+    public MessageResult(MessageStatus status) {
+        super();
+        this.status = status;
     }
 
-    public MessageResult(MessageStatus status, String text) {
+    public MessageResult(MessageStatus status, Message message) {
+        super(message.getDate(), message.getName(), message.getText());
         this.status = status;
-        this.text = text;
+    }
+
+    public MessageResult(MessageStatus status, Date date, String name, String text) {
+        super(date, name, text);
+        this.status = status;
+    }
+
+    public static byte[] serialize(Message message) {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        try (ObjectOutputStream oos = new ObjectOutputStream(baos)) {
+            oos.writeObject(message);
+            return baos.toByteArray();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new byte[] {};
+    }
+
+    public static Message deserialise(byte[] data) {
+        ByteArrayInputStream bais = new ByteArrayInputStream(data);
+        try (ObjectInputStream ois = new ObjectInputStream(bais)) {
+            return (Message)ois.readObject();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
